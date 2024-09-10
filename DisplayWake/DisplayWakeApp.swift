a7a7d6bb25bc688920e0e7d52eaa361e460eb1b3
Eamon Powell <eamonpowell@outlook.com>
2024-09-10 10:27:13 +1000

    Initial Commit

---
+++ DisplayWake/DisplayWakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisplayWakeApp.swift
+//  DisplayWake
+//
+//  Created by Eamon Powell on 10/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct DisplayWakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
